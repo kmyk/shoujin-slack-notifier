180c84bc7d0f5a6feac975940c3c2c75d6cc062b
Kimiyuki Onaka <kimiyuki95@gmail.com>
2018-09-29 04:32:55 +0900

    refactoring

--- shoujin.py
+++ shoujin.py
@@ -17,61 +17,141 @@ def requests_get_json(url):
     return resp.json()
 
 
-def get_data_pair(user, cache_dir):
-    if not re.match('^[0-9A-Za-z]+$', user):
-        raise ValueError()
+class AtCoderShoujin(object):
 
-    # read cache
-    cache_path = pathlib.Path(cache_dir) / (user + '.json')
-    if cache_path.exists():
-        with cache_path.open() as fh:
-            old_data = json.loads(fh.read())
-    else:
-        old_data = []
+    def __init__(self, cache_dir=None):
+        self.cache_dir = cache_dir
+        self.contests = self._get_info_contests()
+        self.problems = self._get_info_merged_problems()
 
-    # use the API
-    url = 'http://kenkoooo.com/atcoder/atcoder-api/results?user=' + user
-    data = requests_get_json(url)
+    def _get_info_contests(self):
+        url = 'http://kenkoooo.com/atcoder/atcoder-api/info/contests'
+        data = requests_get_json(url)
+        f = {}
+        for row in data:
+            f[row['id']] = row
+        return f
 
-    # write cache
-    cache_path.parent.mkdir(parents=True, exist_ok=True)
-    with cache_path.open('w') as fh:
-        fh.write(json.dumps(data))
+    def _get_info_merged_problems(self):
+        url = 'http://kenkoooo.com/atcoder/atcoder-api/info/merged-problems'
+        data = requests_get_json(url)
+        f = {}
+        for row in data:
+            f[row['id']] = row
+        return f
 
-    return { 'new': data, 'old': old_data }
+    def _get_results_pair(self, user_id):
+        if not re.match('^[0-9A-Za-z]+$', user_id):
+            raise ValueError()
+
+        # read cache
+        cache_path = pathlib.Path(self.cache_dir) / 'atcoder' / (user_id + '.json')
+        if cache_path.exists():
+            print('[*] read cache file:', cache_path)
+            with cache_path.open() as fh:
+                old_data = json.loads(fh.read())
+        else:
+            old_data = []
+
+        # use the API
+        url = 'http://kenkoooo.com/atcoder/atcoder-api/results?user=' + user_id
+        data = requests_get_json(url)
+
+        # write cache
+        cache_path.parent.mkdir(parents=True, exist_ok=True)
+        print('[*] write cache file:', cache_path)
+        with cache_path.open('w') as fh:
+            fh.write(json.dumps(data))
+
+        return { 'new': data, 'old': old_data }
+
+    def __call__(self, user_id):  #=> [ { 'title': str, 'url': str, 'score': str (optional) } ]
+        results = self._get_results_pair(user_id)
+        delta = {}
+
+        # add new problems
+        for submission in results['new']:
+            if submission['result'] == 'AC':
+                problem_id = submission['problem_id']
+                contest_id = submission['contest_id']
+                delta[problem_id] = {
+                    'problem_id': problem_id,
+                    'contest_id': contest_id,
+                    'title': self.contests[contest_id]['title'] + ' ' + self.problems[problem_id]['title'],
+                    'url': 'https://beta.atcoder.jp/contests/{}/tasks/{}'.format(contest_id, problem_id),
+                }
+                point = self.problems[problem_id].get('point')
+                if point is not None:
+                    delta[problem_id]['score'] = point
+
+        # remove old problems
+        for submission in results['old']:
+            if submission['result'] == 'AC':
+                if submission['problem_id'] in delta:
+                    del delta[submission['problem_id']]
+
+        delta = list(delta.values())
+        delta.sort(key=lambda problem: problem['problem_id'])
+        return delta
 
 
-def get_accepted_delta(new, old):
-    accepted = set()
-    for row in new:
-        if row['result'] == 'AC':
-            accepted.add(row['problem_id'])
-    for row in old:
-        if row['result'] == 'AC':
-            if row['problem_id'] in accepted:
-                accepted.remove(row['problem_id'])
-    return list(sorted(accepted))
+class CodeforcesShoujin(object):
 
-def get_info_contests():
-    url = 'http://kenkoooo.com/atcoder/atcoder-api/info/contests'
-    data = requests_get_json(url)
-    f = {}
-    for row in data:
-        f[row['id']] = row
-    return f
+    def __init__(self, cache_dir=None):
+        self.cache_dir = cache_dir
 
-def get_info_merged_problems():
-    url = 'http://kenkoooo.com/atcoder/atcoder-api/info/merged-problems'
-    data = requests_get_json(url)
-    f = {}
-    for row in data:
-        f[row['id']] = row
-    return f
+    def __call__(self, user_id):
+        return []
+
+
+def make_data(users, cache_dir=None):
+    services = {
+        'atcoder': AtCoderShoujin(cache_dir=cache_dir),
+        'codeforces': CodeforcesShoujin(cache_dir=cache_dir),
+    }
+
+    # gather data
+    data = []
+    for user_name, user in users.items():
+        problems = []
+        for service_name, user_id in user.items():
+            problems += services[service_name](user_id)
+
+        if not problems:
+            continue
+
+        if len(problems) > 30:
+            text = ''
+        else:
+            texts = []
+            texts += [ '_{}_ solved *{}* problems!'.format(user_name, len(problems)) ]
+            for problem in problems:
+                words = []
+                words += [ problem['title'] ]
+                if 'score' in problem:
+                    words += [ '({} pts)'.format(problem['score']) ]
+                words += [ problem['url'] ]
+                texts += [ ' '.join(words) ]
+            text = '\n'.join(texts)
+
+        data += [ {
+            'name': user_name,
+            'problems': problems,
+            'text': text,
+        } ]
+
+    # sort and concat
+    data.sort(key=lambda row: len(row['problems']), reverse=True)
+    text = '\n\n'.join([ row['text'] for row in data ])
+    return text
 
 
 appname = 'shoujin-slack-notifier'
 default_config = {
-    'users': [ 'kimiyuki', 'tourist' ],
+    'users': {
+        'kimiyuki': { 'atcoder': 'kimiyuki', 'codeforces': 'kimiyuki' },
+        'tourist': { 'atcoder': 'tourist', 'codeforces': 'tourist' },
+    },
     'webhook-url': 'https://hooks.slack.com/services/?????????/?????????/????????????????????????',
     'cache-dir': str(pathlib.Path('~/.cache/{}'.format(appname)).expanduser()),
 }
@@ -88,9 +168,11 @@ def main():
 
     # load the config
     if not args.config_file.exists():
+        print('[*] write config file:', args.config_file)
         args.config_file.parent.mkdir(parents=True, exist_ok=True)
         with args.config_file.open('w') as fh:
             fh.write(json.dumps(default_config))
+    print('[*] read config file:', args.config_file)
     with args.config_file.open() as fh:
         args.config = json.loads(fh.read())
 
@@ -106,33 +188,10 @@ def main():
     print('[*]', datetime.datetime.now())
 
     # make data
-    info_contests = get_info_contests()
-    info_problems = get_info_merged_problems()
-    data = []
-    for user in args.users:
-        accepted = get_data_pair(user, cache_dir=args.cache_dir)
-        delta = get_accepted_delta(new=accepted['new'], old=accepted['old'])
-        if (len(delta) == 0):
-            continue
-        s = ''
-        s += '_{}_ solved *{}* problems!\n'.format(user, len(delta))
-        if len(delta) <= 30:
-            for problem_id in delta:
-                contest_id = info_problems[problem_id]['contest_id']  # TODO: get correct ones even if there are many contests
-                name = info_contests[contest_id]['title'] + ' ' + info_problems[problem_id]['title']
-                point = info_problems[problem_id].get('point')
-                url = 'https://beta.atcoder.jp/contests/{}/tasks/{}'.format(contest_id, problem_id)
-                s += '{} {} {}\n'.format(name, '({} pts)'.format(int(point)) if point else '', url)
-        data += [ {
-            'name': user,
-            'delta': delta,
-            'str': s,
-        } ]
-    data.sort(key=lambda x: len(x['delta']), reverse=True)
-    text = '\n'.join([ row['str'] for row in data ])
+    text = make_data(args.users, cache_dir=args.cache_dir)
 
     # post data
-    if not data:
+    if not text:
         print('[*] nothing to notify...')
     else:
         print('[*] payload:')
